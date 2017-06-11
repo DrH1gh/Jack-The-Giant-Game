d268ecc02ec0b700510f7acdb15046f7da8ab6c0
DrH1gh <alexandru.n.popescu@gmail.com>
2017-06-11 18:39:47 +0300

    cloud collector

---
+++ Assets/Scripts/CloudCollector.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudCollector : Singleton<CloudCollector> {
+
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+        if( target.tag == "Cloud" || target.tag == "DeadlyCloud")
+        {
+            target.gameObject.SetActive(false);
+        }
+    }
+}
