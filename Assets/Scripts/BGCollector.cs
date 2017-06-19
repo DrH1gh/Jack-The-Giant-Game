fada239da8c95c4616dafccd8ed01d7964b67df0
DrH1gh <alexandru.n.popescu@gmail.com>
2017-06-19 22:40:31 +0300

    bg collector & spawner 2/2

--- Assets/Scripts/BGCollector.cs
+++ Assets/Scripts/BGCollector.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BGCollector : MonoBehaviour {
+public class BGCollector : Singleton<BGCollector> {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+        if(target.tag == "Background")
+        {
+            target.gameObject.SetActive(false);
+            //We don't use this like in the CloudCollector because we have the BG in the scene NOT Instantiate them !!
+            //Destroy(target.gameObject);
+        }
+    }
 }
