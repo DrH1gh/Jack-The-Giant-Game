d5d48523246ac44937bf6c79499ecbc476e43626
DrH1gh <alexandru.n.popescu@gmail.com>
2017-06-20 19:00:21 +0300

    playe limits on X axes

---
+++ Assets/Scripts/PlayerLimits.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLimits : MonoBehaviour {
+
+    private float minX, maxX;
+
+
+	// Use this for initialization
+	void Start () {
+        SetMinMax();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(transform.position.x < minX)
+        {
+            Vector3 playerPos = transform.position;
+            playerPos.x = minX;
+            transform.position = playerPos;
+        }
+
+        if(transform.position.x > maxX)
+        {
+            Vector3 playerPos = transform.position;
+            playerPos.x = maxX;
+            transform.position = playerPos;
+        }
+    }
+
+    private void SetMinMax()
+    {
+        Vector3 cameraBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        maxX = cameraBounds.x - 0.5f; 
+        minX = -cameraBounds.x + 0.5f;
+    }
+
+}
