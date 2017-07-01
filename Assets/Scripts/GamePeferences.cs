d5aeecae232846f02c4cdf8180fd93a6343a1468
DrH1gh <alexandru.n.popescu@gmail.com>
2017-07-01 20:59:13 +0300

    game peferences

--- Assets/Scripts/GamePeferences.cs
+++ Assets/Scripts/GamePeferences.cs
@@ -2,15 +2,124 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GamePeferences : Singleton<GamePeferences> {
+public static class GamePeferences
+{
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public static string EasyDiff = "EasyDiff";
+    public static string MediumDiff = "MediumDiff";
+    public static string HardDiff = "HardDiff";
+
+    public static string EasyDiffScore = "EasyDiffScore";
+    public static string MediumDiffScore = "MediumDiffScore";
+    public static string HardDiffScore = "HardDiffScore";
+
+    public static string EasyDiffCoinScore = "EasyDiffCoinScore";
+    public static string MediumDiffCoinScore = "MediumDiffCoinScore";
+    public static string HardDiffCoinScore = "HardDiffCoinScore";
+
+    public static string IsMusicOn = "IsMusicOn";
+
+
+    public static int GetMusic()
+    {
+        return PlayerPrefs.GetInt(IsMusicOn);
+    }
+
+    public static void SetMusic(int state)
+    {
+        PlayerPrefs.SetInt(IsMusicOn, state);
+    }
+
+    public static int GetEasyDifficulty()
+    {
+        return PlayerPrefs.GetInt(EasyDiff);
+    }
+
+    public static void SetEasyDifficulty(int state)
+    {
+        PlayerPrefs.SetInt(EasyDiff, state);
+    }
+
+    public static int GetMediumDifficulty()
+    {
+        return PlayerPrefs.GetInt(MediumDiff);
+    }
+
+    public static void SetMediuDifficulty(int state)
+    {
+        PlayerPrefs.SetInt(MediumDiff, state);
+    }
+
+    public static int GetHardDifficulty()
+    {
+        return PlayerPrefs.GetInt(HardDiff);
+    }
+
+    public static void SetHardDifficulty(int state)
+    {
+        PlayerPrefs.SetInt(HardDiff, state);
+    }
+
+    public static int GetEasyDiffScore()
+    {
+        return PlayerPrefs.GetInt(EasyDiffScore);
+    }
+
+    public static void SetEasyDiffScore(int state)
+    {
+        PlayerPrefs.SetInt(EasyDiffScore, state);
+    }
+
+    public static int GetMediumDiffScore()
+    {
+        return PlayerPrefs.GetInt(MediumDiffScore);
+    }
+
+    public static void SetMediumDiffScore(int state)
+    {
+        PlayerPrefs.SetInt(MediumDiffScore, state);
+    }
+
+    public static int GetHardDiffScore()
+    {
+        return PlayerPrefs.GetInt(HardDiffScore);
+    }
+
+    public static void SetHardDiffScore(int state)
+    {
+        PlayerPrefs.SetInt(HardDiffScore, state);
+    }
+
+    public static int GetEasyDiffCoinScore()
+    {
+        return PlayerPrefs.GetInt(EasyDiffCoinScore);
+    }
+
+    public static void SetEasyDiffCoinScore(int state)
+    {
+        PlayerPrefs.SetInt(EasyDiffCoinScore, state);
+    }
+
+    public static int GetMediumDiffCoinScore()
+    {
+        return PlayerPrefs.GetInt(MediumDiffCoinScore);
+    }
+
+    public static void SetMediumDiffCoinScore(int state)
+    {
+        PlayerPrefs.SetInt(MediumDiffCoinScore, state);
+    }
+
+    public static int GetHardDiffCoinScore()
+    {
+        return PlayerPrefs.GetInt(HardDiffCoinScore);
+    }
+
+    public static void SetHardDiffCoinScore(int state)
+    {
+        PlayerPrefs.SetInt(HardDiffCoinScore, state);
+    }
+
+
+
 }
--- Assets/Scripts/HighScoreController.cs
+++ Assets/Scripts/HighScoreController.cs
@@ -2,13 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class HighScoreController : Singleton<HighScoreController> {
 
+    [SerializeField]
+    private Text scoreText, cointText;
+
     // Use this for initialization
     void Start () {
-		
-	}
+        SetScoreByDifficulty();
+
+    }
+
+    void SetScore(int score, int coinScore)
+    {
+        scoreText.text = score.ToString();
+        cointText.text = coinScore.ToString();
+    }
+
+    void SetScoreByDifficulty()
+    {
+        if(GamePeferences.GetEasyDifficulty() == 1)
+        {
+            SetScore(GamePeferences.GetEasyDiffScore(), GamePeferences.GetEasyDiffCoinScore());
+        }
+
+        if (GamePeferences.GetMediumDifficulty() == 1)
+        {
+            SetScore(GamePeferences.GetMediumDiffScore(), GamePeferences.GetMediumDiffCoinScore());
+        }
+
+        if (GamePeferences.GetHardDifficulty() == 1)
+        {
+            SetScore(GamePeferences.GetHardDiffScore(), GamePeferences.GetHardDiffCoinScore());
+        }
+    }
 	
     public void GoBack()
     {
