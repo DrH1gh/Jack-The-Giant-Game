ec95436f16747e2f4fd2d6320b3f2b8e663558ef
DrH1gh <alexandru.n.popescu@gmail.com>
2017-07-01 14:27:01 +0300

    collectable script

---
+++ Assets/Scripts/CollectableScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableScript : Singleton<CollectableScript>  {
+
+    private void OnEnable()
+    {
+        Invoke("DestryCollectable", 1f);
+    }
+
+    private void OnDisable()
+    {
+
+    }
+
+    private void DestryCollectable()
+    {
+        //Destroy(gameObject);
+        gameObject.SetActive(false);
+    }
+
+}
